de0afc6948d65a554a6897f9a4abd487af4fb593
franciscasotoa <102995723+fransa27@users.noreply.github.com>
2022-07-11 21:55:24 -0400

    checkbox

--- src/components/tareasPersonales.js
+++ src/components/tareasPersonales.js
@@ -1,19 +1,38 @@
-import React , {Component}  from 'react';
+import React , {useState}  from 'react';
 import { View,Text, StyleSheet} from 'react-native';
-
-class TareasPersonales extends Component{
-    render(){
-        return(
-            <View style={styles.container}>
+import CheckBox  from '@react-native-community/checkbox';
+const TareasPersonales= () => {
+    const [isSelect, setSelect] = useState(false);
+    return(
+        <View /* style={styles.container} */>
+            <View style={styles.checkboxContainer}>
+                <CheckBox 
+                    disabled={false}
+                    value={isSelect}
+                    onValueChange={(newValue) => setSelect(newValue)}
+                    style={styles.checkbox}
+                />
                 <Text>Tareas personales en checkbox</Text>
             </View>
-        )
-    }
+            
+        </View>
+    )
+    
 }
 const styles = StyleSheet.create({ 
     container:{
         backgroundColor: '#F5CBA7',
-        
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    checkboxContainer: {
+        flexDirection: "row",
+        marginBottom: 20,
+        backgroundColor: '#F6DDCC'
+    },
+    checkbox: {
+        alignSelf: "center",
     }
 })
 
