b1d848cc2526fdcc3bb0e265202623affea1a3a5
Francisco López <francisco.lopezfi@sansano.usm.cl>
2022-07-16 20:30:49 -0400

    Update tareasEquipo.js

--- frontend/src/components/tareasEquipo.js
+++ frontend/src/components/tareasEquipo.js
@@ -1,6 +1,6 @@
 import React , {Component,useEffect,useState}  from 'react';
 import { View,Text, StyleSheet, FlatList} from 'react-native';
-import ListaTareas from '../components/listaTareas'
+import ListaTareas from './ListaTareas'
 //import {getAllTasks} from '/Users/pancholopez/Documents/1USM/2022-1/TEL335-DAWM/proyecto/TELOrganizo/backend/src/controllers/tareas.controllers'
 //class TareasEquipo extends Component{
    /*  const {
@@ -16,15 +16,20 @@ const TareasEquipo = ()=>{
 
     //render(){
         const [tareas, setTareas] = useState([])
-
+        
         const loadTeamTasks = async ()=>{
-            const data = await fetch('http://localhost:4000/tareas')
-            setTareas(data)
-            console.log(data)
+            
+            const data = await fetch('http://10.0.2.2:4000/tareas')
+            const json_data = await data.json()
+            
+            setTareas(json_data)
+            
         }
         
         useEffect(() => {
+            
           loadTeamTasks()
+          
         
           
         }, [])
@@ -35,7 +40,7 @@ const TareasEquipo = ()=>{
         return(
             <View style={styles.container}>
                 <Text style={styles.title}>Tareas equipo</Text>
-                <Text>Tareas grupo </Text>
+                
                 <ListaTareas tasks={tareas}/>
             </View>
         )
